8ee578b55e0ecc06e7caffa02e128285c638c5c3
Henrique GC <henriq4net@gmail.com>
2023-10-21 16:16:20 -0300

    fix: list image

--- src/components/VoidList.jsx
+++ src/components/VoidList.jsx
@@ -1,9 +1,7 @@
-import List from "./list.png";
-
 export const VoidList = () => {
   return (
     <div className="w-1/2 m-auto flex flex-col justify-center items-center pt-8">
-      <img className="w-12" src={List} alt="" />
+      <img className="w-12" src="/list.png" alt="" />
       <div className="text-gray-500 text-center">
         <p className="font-bold">Você ainda não tem tarefas cadastradas</p>
         <p>Crie tarefas e organize seus itens a fazer</p>
